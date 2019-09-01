a4e044db48053bcf03b16d18856ac1d941553d25
tpluscode <tpluscode@users.noreply.github.com>
2019-09-01 20:48:38 +0200

    ci: update netlify setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,23 @@
 const path = require('path')
 const merge = require('webpack-merge')
-const { createDefaultConfig } = require('@open-wc/building-webpack')
+const { createCompatibilityConfig } = require('@open-wc/building-webpack')
 const CopyPlugin = require('copy-webpack-plugin')
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// const { createCompatibilityConfig } = require('@open-wc/building-webpack');
-// module.exports = createCompatibilityConfig({
-//   input: path.resolve(__dirname, './index.html'),
-// });
+const configs = createCompatibilityConfig({
+  input: path.resolve(__dirname, './src/demo/index.html'),
+})
 
-module.exports = merge(
-  createDefaultConfig({
-    input: path.resolve(__dirname, './src/demo/index.html'),
-  }),
-  {
+module.exports = configs.map(config =>
+  merge(config, {
     output: {
       path: path.resolve(__dirname, './demo'),
     },
-    plugins: [
-      new CopyPlugin([{ from: 'src/demo/pages', to: 'pages' }, { from: 'src/*.html', to: '.' }]),
-    ],
+    plugins: [new CopyPlugin([{ from: 'src/demo/pages', to: 'pages' }])],
     resolve: {
       extensions: ['.ts', '.js', '.json'],
     },
     devServer: {
       publicPath: '/demo/',
-      historyApiFallback: {
-        rewrites: [{ from: /^\/$/, to: '/demo/index.html' }],
-      },
     },
-  },
+  }),
 )
